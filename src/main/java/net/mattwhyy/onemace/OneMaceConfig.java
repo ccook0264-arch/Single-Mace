c7bddf052f1e8dbeb373fe830ff4430cb5eb439a
Carter Cook <ccook0264@gmail.com>
2025-10-19 14:18:09 -0600

    Refactor OneMaceConfig for improved file handling

--- src/main/java/net/mattwhyy/onemace/OneMaceConfig.java
+++ src/main/java/net/mattwhyy/onemace/OneMaceConfig.java
@@ -2,18 +2,18 @@ package net.mattwhyy.onemace;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import net.fabricmc.loader.api.FabricLoader;
-
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class OneMaceConfig {
-    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final Gson GSON = (new GsonBuilder()).setPrettyPrinting().create();
     private static final Path CONFIG_PATH = FabricLoader.getInstance().getConfigDir().resolve("onemace.json");
-
     public boolean maceCrafted = false;
     public String maceOwner = null;
     public boolean announceMaceMessages = true;
@@ -21,38 +21,36 @@ public class OneMaceConfig {
     public boolean allowLocateForAll = false;
     public boolean coloredName = false;
     public String maceNameColor = "RED";
-
-    public Map<String, Boolean> offlineInventory = new HashMap<>();
-
-    public Messages messages = new Messages();
-
-    public static class Messages {
-        public String crafted = "&b[OneMace] &eThe Mace has been crafted!";
-        public String lost = "&b[OneMace] &eThe Mace has been lost!";
-    }
-
-    // Current instance
+    public Map<String, Boolean> offlineInventory = new HashMap();
+    public OneMaceConfig.Messages messages = new OneMaceConfig.Messages();
     public static OneMaceConfig INSTANCE = new OneMaceConfig();
 
     public static void load() {
         try {
-            if (Files.exists(CONFIG_PATH)) {
+            if (Files.exists(CONFIG_PATH, new LinkOption[0])) {
                 String json = Files.readString(CONFIG_PATH);
-                INSTANCE = GSON.fromJson(json, OneMaceConfig.class);
+                INSTANCE = (OneMaceConfig)GSON.fromJson(json, OneMaceConfig.class);
             } else {
-                save(); // write defaults
+                save();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException var1) {
+            var1.printStackTrace();
         }
+
     }
 
     public static void save() {
         try {
             String json = GSON.toJson(INSTANCE);
-            Files.writeString(CONFIG_PATH, json);
-        } catch (IOException e) {
-            e.printStackTrace();
+            Files.writeString(CONFIG_PATH, json, new OpenOption[0]);
+        } catch (IOException var1) {
+            var1.printStackTrace();
         }
+
+    }
+
+    public static class Messages {
+        public String crafted = "&b[OneMace] &eThe Mace has been crafted!";
+        public String lost = "&b[OneMace] &eThe Mace has been lost!";
     }
 }
